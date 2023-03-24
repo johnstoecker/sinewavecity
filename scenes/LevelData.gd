de263d705434c19ebc1785c9d9351d9fa5136e52
John Stoecker <john.stoecker@clever.com>
2023-03-24 12:04:09 -0700

    adding files, adding some level logic

--- scenes/LevelData.gd
+++ scenes/LevelData.gd
@@ -1,7 +1,7 @@
 extends Node
 
 
-enum {STATE_IDLE, STATE_RANDOM_SPAWN, STATE_CONCURRENT_SPAWN, STATE_SWEEP}
+enum {STATE_IDLE, STATE_RANDOM_SPAWN, STATE_SWEEP, MIDDLE_PULSE_RANDOM_SPAWN}
 
 var lastSpawn = 0;
 
@@ -11,16 +11,39 @@ var levelStart = 0;
 var currentLevel = 0;
 var currentStep = 0;
 
+# if we need to do any init on the next state, set to false
+# (set sweep direction, etc)
+var isStateInit = false;
+
 var currentState = STATE_IDLE;
 
 var rng = RandomNumberGenerator.new()
 
 var vertBombScene = preload("res://objects/VertBomb.tscn")
 
+
 var levels = [
 	[
-		{ "ends": 1000, "state": STATE_IDLE },
-		{ "ends": 10100, "state": STATE_RANDOM_SPAWN, "spawnRate": 1000 },
+		# a few lasers
+		{ "ends": 1000, "state": STATE_RANDOM_SPAWN, "maxConcurrentLasers": 1, "spawnRate": 1000 },
+		# more, sometimes 2 at once
+		{ "ends": 10000, "state": STATE_RANDOM_SPAWN, "maxConcurrentLasers": 2,  "spawnRate": 1000 },
+		# more, sometimes 3 at once
+		{ "ends": 20000, "state": STATE_RANDOM_SPAWN, "maxConcurrentLasers": 3,  "spawnRate": 1000 },
+		# rising action
+		{ "ends": 30000, "state": STATE_RANDOM_SPAWN, "maxConcurrentLasers": 3,  "spawnRate": 500 },
+		# going for it -- sweeps, 12 pulses per second
+		{ "ends": 40000, "state": STATE_SWEEP, "spawnRate": 150 },
+		# double sweeps
+		{ "ends": 50000, "state": STATE_SWEEP, "spawnRate": 150 },
+		# bunch of lasers, sometimes 3 at once
+		{ "ends": 60000, "state": STATE_RANDOM_SPAWN, "maxConcurrentLasers": 3, "spawnRate": 500 },
+		# middle pullse and side lasers
+		{ "ends": 70000, "state": MIDDLE_PULSE_RANDOM_SPAWN, "spawnRate": 500 },
+		# more sweeps
+		{ "ends": 80000, "state": STATE_SWEEP, "spawnRate": 150 },
+		# spawn 3-5 at once
+		{ "ends": 90000, "state": STATE_RANDOM_SPAWN, "maxConcurrentLasers": 5, "minConcurrentLasers": 3, "spawnRate": 500},
 		{ "ends": 20000, "state": STATE_IDLE }
 	]
 ]
@@ -36,6 +59,7 @@ func startLevel(level):
 	currentStep = 0;
 	currentState = STATE_IDLE;
 	lastSpawn = 0;
+	isStateInit = false;
 	
 	
 func _process(delta):
@@ -44,6 +68,7 @@ func _process(delta):
 	if timeElapsed > levels[currentLevel][currentStep]["ends"]:
 		print("switching to new state")
 		currentStep += 1
+		isStateInit = true;
 		if currentStep >= levels[currentLevel].size():
 			currentState = STATE_IDLE
 			# TODO: go to next level
@@ -57,10 +82,46 @@ func _process(delta):
 		doRandomSpawn(timeElapsed)
 	elif currentState == STATE_SWEEP:
 		doSweep(timeElapsed)
+	elif currentState == MIDDLE_PULSE_RANDOM_SPAWN:
+		doMiddlePulseRandomSpawn(timeElapsed)
+	
 	
 	
 func doRandomSpawn(timeElapsed):
+#    var x_1 = random(global.area_width);
+#    var x_2 = random(global.area_width);
+#    var x_begin = min(x_1, x_2);
+#    var x_end = max(x_1, x_2);
+#    var width = x_end - x_begin;
+#    //how many lasers to spawn
+#    var num = random(2)+2;
+#    if (width &lt; num * global.min_sine_width) {
+#        num = 1;
+#    }
+ #   {
+ #   var i;
+ #   for (i = 0; i &lt; num; i += 1)
+ #      {
+ #      instance_create(width/num * i + x_begin, 0, obj_mine);
+ #      }
+ #   }
 	if timeElapsed - lastSpawn > levels[currentLevel][currentStep]["spawnRate"]:
+		var minConcurrentLasers = 1
+#		if levels[currentLevel][currentStep]["minConcurrentLasers"] != null:
+#			minConcurrentLasers = levels[currentLevel][currentStep]["minConcurrentLasers"]
+		var maxConcurrentLasers = 1
+		if levels[currentLevel][currentStep]["maxConcurrentLasers"] != null:
+			maxConcurrentLasers = levels[currentLevel][currentStep]["maxConcurrentLasers"]
+		# how many mines to spawn
+		var num = rng.randi_range(minConcurrentLasers, maxConcurrentLasers)
+
+		var x1 = rng.randi_range(0,get_viewport().size.x)
+		var x2 = rng.randi_range(0,get_viewport().size.x)
+		var xLeft = min(x1, x2)
+		var xRight = max(x1, x2)
+		var width = xRight - xLeft
+		#TODO: finish spawning multiple, ensure width is not too smallish
+		#if width - 
 		print("Spawning")
 		var newBomb = vertBombScene.instance()
 		newBomb.position.x = rng.randi_range(200,get_viewport().size.x - 200)
@@ -69,4 +130,7 @@ func doRandomSpawn(timeElapsed):
 	
 
 func doSweep(timeElapsed):
+	pass
+
+func doMiddlePulseRandomSpawn(timeElapsed):
 	pass
